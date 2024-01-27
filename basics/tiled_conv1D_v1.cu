e652c193ae33da44174f9e845124f270a7f8ab6a
xishou <xishou@pinduoduo.com>
2024-01-27 14:17:19 +0800

    unify code

--- basics/tiled_conv1D_v1.cu
+++ basics/tiled_conv1D_v1.cu
@@ -6,7 +6,7 @@
 #define MASK_WIDTH 5
 #define RADIUS MASK_WIDTH / 2
 // 每个tile需要写的数量
-#define O_TILE_WIDTH 10 
+#define O_TILE_WIDTH 256
 // 每个tile需要读的数量，需要额外读取更多数据，所以block内的线程数多于每个block需要写的量，一部分线程只负责读数据
 #define BLOCK_WIDTH (O_TILE_WIDTH + MASK_WIDTH - 1) 
 
@@ -14,10 +14,10 @@
 __constant__ int M[MASK_WIDTH];
 
 
-__global__ void tiled1DConvKernel(int *N, int *P, int width) {
+__global__ void tiled1DConvKernelV2(int *N, int *P, int width) {
     // 共享内存
-    __shared__ int Ns[BLOCK_WIDTH];  
-
+    //__shared__ int Ns[BLOCK_WIDTH];  
+    extern __shared__ int Ns[];
     // 当前线程负责写的位置
     int index_o = blockIdx.x * O_TILE_WIDTH + threadIdx.x;
     // 当前线程负责读的位置
@@ -59,40 +59,42 @@ void verify_result(int *array, int *result, int *mask, int n) {
         for (int j = 0; j < MASK_WIDTH; j++) {
             temp += h_array[i + j] * mask[j];
         }
-        //if (i < 20) {
-        //  printf("temp=%d, result=%d\n", temp, result[i]);
-        //}
-        //if (temp != result[i]) {
-        //printf("i=%d, temp=%d, result=%d\n", i, temp, result[i]);
-        //}
+        
         assert(temp == result[i]);
     }
 }
 
-int main(void) {
+int main() {
     setGPU();
-
     // 数组长度
     int width = 1 << 20;
 
-    int *A, *P;
+    int *h_array = new int[width];
+
     int nBytes = width * sizeof(int);
     int mBytes = MASK_WIDTH * sizeof(int);
-
     // 内存托管
-    cudaMallocManaged((void**)&A, nBytes);
-    cudaMallocManaged((void**)&P, nBytes);
+    //ErrorCheck(cudaMallocManaged((void**)&A, nBytes),  __FILE__, __LINE__);
+    //ErrorCheck(cudaMallocManaged((void**)&P, nBytes),  __FILE__, __LINE__);
 
     // 初始化数据
     for (int i = 0; i < width; i++) {
-        A[i] = rand() % 100;
+        h_array[i] = rand() % 100;
     }
-
     // 初始化mask
     int *h_mask = new int[MASK_WIDTH];
     for (int i = 0; i < MASK_WIDTH; i++) {
         h_mask[i] = rand() % 10;
     }
+
+    int *h_result = new int[width];
+    int *d_array, *d_result;
+    ErrorCheck(cudaMalloc(&d_array, nBytes), __FILE__, __LINE__);
+    ErrorCheck(cudaMalloc(&d_result, nBytes), __FILE__, __LINE__);
+
+    // 将A的元素copy到DRAM
+    ErrorCheck(cudaMemcpy(d_array, h_array, nBytes, cudaMemcpyHostToDevice), __FILE__, __LINE__);
+
     // 将数据copy到常量内存
     cudaMemcpyToSymbol(M, h_mask, mBytes);
 
@@ -101,17 +103,20 @@ int main(void) {
     // 每个block需要写O_TILE_WIDTH个数据
     dim3 gridSize((width + O_TILE_WIDTH - 1) / O_TILE_WIDTH, 1);
     // 执行kernel
-    tiled1DConvKernel <<< gridSize, blockSize >>>(A, P, width);
+    size_t SHMEM = (BLOCK_WIDTH + RADIUS * 2) * sizeof(int);
+    tiled1DConvKernelV2<<<gridSize, blockSize, SHMEM>>>(d_array, d_result, width);
+
+    // 将执行结果copy到主机
+    ErrorCheck(cudaMemcpy(h_result, d_result, nBytes, cudaMemcpyDeviceToHost), __FILE__, __LINE__);
     // 等待所有线程执行完毕
     //cudaDeviceSynchronize();
     // 检查结果
-    /**
-    for (int i = 0; i < width; i++) {
-        printf("id=%d, P=%d\n", i, P[i]);
-    }
-    **/
-    verify_result(A, P, h_mask, width);
+    verify_result(h_array, h_result, h_mask, width);
     std::cout << "COMPLETED SUCCESSFULLY\n";
+    delete[] h_array;
+    delete[] h_result;
+    delete[] h_mask;
+    cudaFree(d_result);
 
     return 0;
 }
